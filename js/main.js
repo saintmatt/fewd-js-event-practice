8aca3cac0d77e2c5eed39cc8f3b47b23f0a672f2
Mattie <sultana95@gmail.com>
2020-03-10 20:36:06 -0400

    added directions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
-(function () {
 
-  // your js goes here
+var button1 = document.querySelector('button');
 
-})();
+var button2 = document.querySelector('.button-success');
+
+var button3 = document.querySelector('.button-warning');
+
+document.addEventListener('click', function(Event) {
+  if (event.target === button1) {
+    button1.classList.toggle('button-danger');
+
+  }
+
+  if (event.target === button2) {
+    button2.innerText = 'You Clicked Me!';
+
+  }
+
+  if (event.target === button3) {
+    button2.innerText = 'button two'
+
+  }
+
+});
